67746a251bd2538084ecbdea0d15e12c83bbcb31
tzorca <tzorca@users.noreply.github.com>
2015-02-16 22:42:05 -0500

    Used regex to get header separator line.

--- race-result-parser.py
+++ race-result-parser.py
@@ -1,9 +1,24 @@
 import os
+import re
+
+RESULTS_HEADER_SEPARATOR_PATTERN = re.compile(r'(=+ ?)+')
+
+def parse_results(data):
+    lines = data.splitlines(True)
+    for line in lines:
+        match = RESULTS_HEADER_SEPARATOR_PATTERN.match(line)
+
+        if (match is not None):
+            print(match.group())
+
 
 DATA_DIRECTORY = "./data/"
-
-data_files = os.listdir(DATA_DIRECTORY)
-
-for filename in data_files:
+for filename in os.listdir(DATA_DIRECTORY):
     with open(DATA_DIRECTORY + filename, "r") as f:
-        print(f.read())
+        parse_results(f.read())
+
+
+
+
+
+
