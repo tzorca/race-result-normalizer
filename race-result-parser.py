d8cc357b9fa693c2298e942528bc724eb84d1d4f
tzorca <tzorca@users.noreply.github.com>
2015-02-16 22:51:39 -0500

    Parsed header by looking at line before header separator.

--- race-result-parser.py
+++ race-result-parser.py
@@ -3,14 +3,29 @@ import re
 
 RESULTS_HEADER_SEPARATOR_PATTERN = re.compile(r'(=+ ?)+')
 
+# Place Name                     Bib#  Age S Guntime  Pace     
+# ===== ======================== ===== === = =======  ===== 
+
 def parse_results(data):
+    header_lines = parse_header(data)
+
+    print header_lines
+
+def parse_header(data):
+    header_lines = None
+
+    line_num = 0
     lines = data.splitlines(True)
     for line in lines:
         match = RESULTS_HEADER_SEPARATOR_PATTERN.match(line)
 
         if (match is not None):
-            print(match.group())
+            header_lines = [lines[line_num-1], line]
+            break
 
+        line_num += 1
+
+    return header_lines
 
 DATA_DIRECTORY = "./data/"
 for filename in os.listdir(DATA_DIRECTORY):
