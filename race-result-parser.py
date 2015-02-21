53d2568cfca9e176fca8959c34b232304f2e7041
tzorca <tzorca@users.noreply.github.com>
2015-02-20 20:46:10 -0500

    Finished implementing function to filter to result lines.

--- race-result-parser.py
+++ race-result-parser.py
@@ -3,6 +3,11 @@ import re
 
 RESULTS_HEADER_SEPARATOR_PATTERN = re.compile(r'(=+ ?)+')
 
+NON_RESULT_PATTERNS = [
+    re.compile(r'\*{4,}'),
+    re.compile(r' {30,}')
+]
+
 def parse_results(raw_data):
     header_lines = get_header(raw_data)
     field_defs = get_field_defs(header_lines)
@@ -47,19 +52,27 @@ def get_field_defs(header_lines):
 
     return field_defs
 
-# Removes lines that aren't the right length or that have significantly different symbol distributions
+
+# Removes non-result lines
 def filter_to_result_lines(header_lines, raw_data):
     lines = raw_data.splitlines(True)
-    result_lines = []
+    temp_lines = []
 
     # Start by eliminating header lines
-    result_lines = [line for line in lines if any(line != header for header in header_lines)]
+    temp_lines = [line for line in lines if not any(line == header for header in header_lines)]
 
     # Next, save only lines that are the same length as a header line
     good_lengths = map(len, header_lines)
-    result_lines = [line for line in result_lines if any(len(line) == length for length in good_lengths)]
+    temp_lines = [line for line in temp_lines if any(len(line) == length for length in good_lengths)]
 
-    # TODO: Finally, calculate and compare symbol distributions
+    # Finally, remove lines matching known non-results patterns
+    result_lines = []
+    for line in temp_lines:
+
+        if (any(pattern.search(line) != None for pattern in NON_RESULT_PATTERNS)):
+            continue
+
+        result_lines.append(line)
 
     return result_lines
 
